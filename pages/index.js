9467a8698386a43be5f9d60038dd4f0cabc207f1
Tu Ha Dinh <hadinhtu97@gmail.com>
2021-03-03 02:54:13 +0700

    Show index page with styless

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,54 @@
 import styles from '../styles/Home.module.css'
+import React, { useState } from 'react'
+import Head from 'next/head'
+
+import fetch from 'node-fetch'
 
 const Home = () => {
+    const [search, setSearch] = useState('')
+    const [data, setData] = useState(null)
+
+    const getDataFromApiThenSetToState = async (e) => {
+        e.preventDefault()
+        if (search == '') {
+            setData(null)
+        } else {
+            fetch('/api/search?s=' + search)
+                .then(res => res.json())
+                .then(data => {
+                    if (data.hasOwnProperty('error')) {
+                        setData(null)
+                    } else {
+                        setData(data)
+                    }
+                })
+        }
+    }
+
     return (
-        <h1 className={styles.home}>Next temp</h1>
+        <>
+            <Head>
+                <title>Wikipedia Viewer</title>
+            </Head>
+            <main>
+                <a href="https://en.wikipedia.org/wiki/Special:Random" target="_blank">Random wiki</a>
+                <form>
+                    <input type='text' value={search} onChange={e => setSearch(e.target.value)} />
+                    <input type='submit' value="Send" onClick={getDataFromApiThenSetToState} />
+                </form>
+                <div>
+                    {
+                        data == null ? <></> :
+                            data.map(d =>
+                                <a href={d.url} target='_blank' key={d.url}>
+                                    <p key={d.title}>{d.title}</p>
+                                    <p key={d.description}>{d.description}</p>
+                                </a>
+                            )
+                    }
+                </div>
+            </main>
+        </>
     )
 }
 
