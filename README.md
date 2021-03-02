cfa85e168238120fb8f3ddd5c76c0b5eb320baec
Tu Ha Dinh <53150506+hadinhtu97@users.noreply.github.com>
2021-03-03 00:10:10 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Next template
+
+## Description
+
+
+## Use
+```
+git clone https://github.com/hadinhtu97/abc
+cd abc
+npm install
+npm run build
+npm run start
+```
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import styles from '../styles/Home.module.css'
+
+const Home = () => {
+    return (
+        <h1 className={styles.home}>Next temp</h1>
+    )
+}
+
+export default Home
