360d00eea8e735323fb5c3e58891b6ebc8852e99
Tu Ha Dinh <hadinhtu97@gmail.com>
2021-03-03 04:32:56 +0700

    Clear data when clear input form

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import styles from '../styles/Home.module.css'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Head from 'next/head'
 
 import fetch from 'node-fetch'
@@ -7,6 +7,12 @@ import fetch from 'node-fetch'
 const Home = () => {
     const [search, setSearch] = useState('')
     const [data, setData] = useState(null)
+
+    useEffect(() => {
+        if (search == '') {
+            setData(null)
+        }
+    }, [search])
 
     const getDataFromApiThenSetToState = async (e) => {
         setData(null)
@@ -33,7 +39,7 @@ const Home = () => {
             </Head>
             <main className={styles.container}>
                 <form className={styles.form}>
-                    <input type='text' value={search} onChange={e => setSearch(e.target.value)} className={styles.searchInput} placeholder='press enter to search'/>
+                    <input type='search' value={search} onChange={e => setSearch(e.target.value)} className={styles.searchInput} placeholder='press enter to search' />
                     <input type='submit' value="" onClick={getDataFromApiThenSetToState} className={styles.submitInput} />
                 </form>
                 <a href="https://en.wikipedia.org/wiki/Special:Random" target="_blank" className={styles.randomLink}>go to a random wiki</a>
