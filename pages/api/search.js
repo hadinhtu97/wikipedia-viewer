5dad2f0d13bb2fc2b133e8501d2a736c8c953169
Tu Ha Dinh <hadinhtu97@gmail.com>
2021-03-03 01:54:50 +0700

    Change the query variable name to s

--- pages/api/search.js
+++ pages/api/search.js
@@ -2,12 +2,12 @@ import Search from '../../modules/search'
 
 export default async (req, res) => {
     if (req.method == 'GET') {
-        if (req.query.search == undefined) {
+        if (req.query.s == undefined) {
             res.send('Required search query!')
         } else {
-            let data = await Search(req.query.search);
+            let data = await Search(req.query.s);
             if (data == null) {
-                res.send('Do not match any page in Wikipedia with search: ' + req.query.search)
+                res.send('Do not match any page in Wikipedia with search: ' + req.query.s)
             } else {
                 res.json(data)
             }
