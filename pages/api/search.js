c3203f066dd8879ca86990a1106baba68a24d5c5
Tu Ha Dinh <hadinhtu97@gmail.com>
2021-03-03 02:48:56 +0700

    Api will responsive a object instead of a string when got a error

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,19 +1,19 @@
-import Search from '../../modules/search'
+import Search from '../../lib/search'
 
 export default async (req, res) => {
     if (req.method == 'GET') {
         if (req.query.s == undefined) {
-            res.send('Required s query!')
+            res.send('Required "s" query!')
         } else {
             try {
                 let data = await Search(req.query.s);
                 if (data == null) {
-                    res.send('Do not match any page in Wikipedia with search: ' + req.query.s)
+                    res.json({ error: 'Do not match any page in Wikipedia with search: ' + req.query.s })
                 } else {
                     res.json(data)
                 }
             } catch (err) {
-                res.send('Some error happened. Try again latter.')
+                res.json({ error: 'Some error happened. Try again latter.' })
             }
         }
     } else {
