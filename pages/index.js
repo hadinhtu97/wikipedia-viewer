c873625e0b45957b353bab54bf2d52ae177f4b4f
Tu Ha Dinh <hadinhtu97@gmail.com>
2021-03-03 04:01:57 +0700

    Style index page

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ const Home = () => {
     const [data, setData] = useState(null)
 
     const getDataFromApiThenSetToState = async (e) => {
+        setData(null)
         e.preventDefault()
         if (search == '') {
             setData(null)
@@ -30,19 +31,19 @@ const Home = () => {
             <Head>
                 <title>Wikipedia Viewer</title>
             </Head>
-            <main>
-                <a href="https://en.wikipedia.org/wiki/Special:Random" target="_blank">Random wiki</a>
-                <form>
-                    <input type='text' value={search} onChange={e => setSearch(e.target.value)} />
-                    <input type='submit' value="Send" onClick={getDataFromApiThenSetToState} />
+            <main className={styles.container}>
+                <form className={styles.form}>
+                    <input type='text' value={search} onChange={e => setSearch(e.target.value)} className={styles.searchInput} placeholder='press enter to search'/>
+                    <input type='submit' value="" onClick={getDataFromApiThenSetToState} className={styles.submitInput} />
                 </form>
-                <div>
+                <a href="https://en.wikipedia.org/wiki/Special:Random" target="_blank" className={styles.randomLink}>go to a random wiki</a>
+                <div className={styles.contents}>
                     {
                         data == null ? <></> :
                             data.map(d =>
-                                <a href={d.url} target='_blank' key={d.url}>
-                                    <p key={d.title}>{d.title}</p>
-                                    <p key={d.description}>{d.description}</p>
+                                <a className={styles.content} href={d.url} target='_blank' key={d.url} title="View on Wikipedia!">
+                                    <p className={styles.title} key={d.title}>{d.title}</p>
+                                    <p className={styles.description} key={d.description}>{d.description}</p>
                                 </a>
                             )
                     }
